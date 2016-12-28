de9d8d2bff7714470ae098d06c6c6833117c5472
pranithmididoddi <pranith13@gmail.com>
2016-12-27 21:38:11 -0600

    Inorder Traversal

--- src/Solution.java
+++ src/Solution.java
@@ -64,4 +64,24 @@ public class Solution {
 
         return true;
     }
+
+    /**SortedArray to binary search tree*/
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if(nums.length==0) return null;
+
+        return sortedArrayToBST(nums,0,nums.length-1);
+    }
+
+    public TreeNode sortedArrayToBST(int[] nums, int start, int end){
+        if(start>end) return null;
+
+        int mid=(start+end)/2;
+        TreeNode root=new TreeNode(nums[mid]);
+
+        root.left=sortedArrayToBST(nums,start,mid-1);
+        root.right=sortedArrayToBST(nums,mid+1,end);
+
+        return root;
+
+    }
 }
