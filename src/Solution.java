0b2959ed6d1601f123477dbf06e8219f79788f1a
pranithmididoddi <pranith13@gmail.com>
2016-12-27 21:48:16 -0600

    Inorder Traversal

--- src/Solution.java
+++ src/Solution.java
@@ -84,4 +84,25 @@ public class Solution {
         return root;
 
     }
+
+    /**List to BST*/
+    public TreeNode sortedArrayToBST(final List<Integer> a) {
+        if(a.isEmpty()) return null;
+
+        int size=a.size();
+
+        return sortedArrayToBST(a,0,size-1);
+    }
+
+    public TreeNode sortedArrayToBST(List<Integer> a, int start, int end){
+        if(start>end) return null;
+
+        int mid=(start+end)/2;
+        TreeNode root=new TreeNode(a.get(mid));
+
+        root.left=sortedArrayToBST(a, start, mid-1);
+        root.right=sortedArrayToBST(a, mid+1, end);
+
+        return root;
+    }
 }
