a01b3d4647b1216c6689a89c033a9807a301bf50
Srinivas <pranith13@gmail.com>
2017-02-26 22:43:20 -0600

    level order ii (bottom-up)

--- src/Solution.java
+++ src/Solution.java
@@ -409,5 +409,37 @@ public class Solution {
         return root;
     }
 
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+
+        List<List<Integer>> result=new ArrayList<>();
+
+        if(root==null) return result;
+
+        Queue<TreeNode> queue=new LinkedList<>();
+        queue.offer(root);
+
+        while(!queue.isEmpty()){
+            int size=queue.size();
+            List<Integer> list=new ArrayList<>();
+
+            for(int i=0;i<size;i++){
+                TreeNode temp=queue.poll();
+                list.add(temp.val);
+
+
+                if(temp.left!=null){
+                    queue.offer(temp.left);
+                }
+                if(temp.right!=null){
+                    queue.offer(temp.right);
+                }
+            }
+            result.add(list);
+        }
+        Collections.reverse(result);
+
+        return result;
+    }
+
 
 }
