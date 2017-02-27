a47b73eddf53a85858b529db75a2acba2822303c
Srinivas <pranith13@gmail.com>
2017-02-26 22:56:36 -0600

    zig zag level order

--- src/Solution.java
+++ src/Solution.java
@@ -441,5 +441,43 @@ public class Solution {
         return result;
     }
 
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+
+        int count=0;
+        List<List<Integer>> result=new ArrayList<>();
+
+        if(root==null) return result;
+
+        Queue<TreeNode> queue=new LinkedList<>();
+        queue.offer(root);
+
+        while(!queue.isEmpty()){
+            int size=queue.size();
+            List<Integer> list=new ArrayList<>();
+
+            for(int i=0;i<size;i++){
+                TreeNode temp=queue.poll();
+                list.add(temp.val);
+
+
+                if(temp.left!=null){
+                    queue.offer(temp.left);
+                }
+                if(temp.right!=null){
+                    queue.offer(temp.right);
+                }
+            }
+            result.add(list);
+        }
+
+        for(List l : result){
+            if(count%2!=0){
+                Collections.reverse(l);
+            }
+            count++;
+        }
+        return result;
+    }
+
 
 }
