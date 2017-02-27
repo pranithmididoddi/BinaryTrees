e4c17df63334ca732397b72fce33c894ceb8af46
Srinivas <pranith13@gmail.com>
2017-02-27 12:47:53 -0600

    zig zag level order

--- src/Solution.java
+++ src/Solution.java
@@ -526,5 +526,36 @@ public class Solution {
         return list;
     }
 
+    public List<Integer> rightSideView(TreeNode root) {
+
+        List<Integer> list=new ArrayList<>();
+        if(root==null) return list;
+
+        Queue<TreeNode> queue=new LinkedList<>();
+
+        queue.add(root);
+
+        while(queue.size()>0){
+            int size=queue.size();
+
+            for(int i=0;i<size;i++){
+                TreeNode temp=queue.remove();
+                if(i==0){
+                    int value=temp.val;
+                    list.add(value);
+                }
+
+                if(temp.right!=null){
+                    queue.add(temp.right);
+                }
+
+                if(temp.left!=null){
+                    queue.add(temp.left);
+                }
+            }
+        }
+        return list;
+    }
+
 
 }
