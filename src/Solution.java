f1902aca0ba4ee4be6adeb0a49ccc3c0725fe13c
Srinivas <pranith13@gmail.com>
2017-03-06 23:59:41 -0600

    count the number of nodes

--- src/Solution.java
+++ src/Solution.java
@@ -645,4 +645,46 @@ public class Solution {
         return count;
     }
 
+    public TreeNode deserialize(String data) {
+        if(data==null || data.length()==0)
+            return null;
+
+        String[] arr = data.split(",");
+        TreeNode root = new TreeNode(Integer.parseInt(arr[0]));
+
+
+        LinkedList<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+
+        int i=1;
+        while(!queue.isEmpty()){
+            TreeNode t = queue.poll();
+
+            if(t==null)
+                continue;
+
+            if(!arr[i].equals("#")){
+                t.left = new TreeNode(Integer.parseInt(arr[i]));
+                queue.offer(t.left);
+
+            }else{
+                t.left = null;
+                queue.offer(null);
+            }
+            i++;
+
+            if(!arr[i].equals("#")){
+                t.right = new TreeNode(Integer.parseInt(arr[i]));
+                queue.offer(t.right);
+
+            }else{
+                t.right = null;
+                queue.offer(null);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
 }
