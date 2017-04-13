27e86c71bd66b4dd980e89b7631efab03711de01
Srinivas <pranith13@gmail.com>
2017-04-12 21:56:50 -0500

    Binary Tree Implementation

---
+++ src/BT.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Pranith on 4/12/17.
+ */
+public class BT {
+}
--- src/BTNode.java
+++ src/BTNode.java
@@ -2,4 +2,39 @@
  * Created by Pranith on 4/11/17.
  */
 public class BTNode {
+
+    BTNode left, right;
+    int data;
+
+    public BTNode(){
+        left=null;
+        right=null;
+        data=0;
+    }
+
+    public BTNode(int n){
+        left=null;
+        right=null;
+        data=n;
+    }
+
+    public void setLeft(BTNode n){
+        left=n;
+    }
+
+    public void setRight(BTNode n){
+        right=n;
+    }
+
+    public BTNode getLeft(){
+        return left;
+    }
+
+    public BTNode getRight(){
+        return right;
+    }
+
+    public int getData(){
+        return data;
+    }
 }
